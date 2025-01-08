f732c6ac6ee71cb4996e81d0df359b81a22c1bde
vaibhav kamble <vaibhavkamble023@gmail.com>
2025-01-08 22:21:18 +0530

    included body parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
